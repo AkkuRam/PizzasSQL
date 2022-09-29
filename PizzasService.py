92f1476964a1c1b1d36c957f3e6930690b848836
Akku Ram <reachakhil10@gmail.com>
2022-09-29 20:42:22 +0200

    new classes

--- PizzasService.py
+++ PizzasService.py
@@ -1,34 +1,54 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine("mysql://root:123@localhost:3306/pizza_service", echo = True)
 base = declarative_base()
 connection = engine.connect()
 
 
-class Pizzas(base):
+class Pizza(base):
 
-    __tablename__ = 'Pizzas'
+    __tablename__ = 'pizza'
 
-    pizza_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key = True)
     pizza_name = Column(String(255))
     price = Column(Integer)
+  
 
     
 class Ingredients(base):
 
-    __tablename__ = 'Ingredients'
+    __tablename__ = 'ingredients'
 
-    ingredient_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key = True)
     ingredient_name = Column(String(255))
     price = Column(Integer)
 
 class Order(base):
+
+    __tablename__ = 'order'
+
+    order_id = Column(Integer, primary_key = True)
+    pizza_id = Column(Integer, ForeignKey('pizza.id'))
+    pizza = relationship("Pizza")
+
+class Customer(base):
+
+    __tablename__ = 'customer'
+
+    name = Column(String(255))
+    address_id = Column(Integer, ForeignKey('address.id'))
+    address = relationship("Address")
+
+
+class Address(base):
+    __tablename__ = 'address'
+
+    id = Column(Integer, primary_key = True)
+    city_name = Column(String(255))
+    street_name = Column(String(255))
+    zip_code = Column(String(255))
     
-    __tablename__ = ''
-
-
-
 base.metadata.create_all(engine)
