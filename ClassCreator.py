e0acc50782ca3fe2ee51e227f35e632f153ae6ab
YungSpecht <burneraee@gmail.com>
2022-10-07 11:30:27 +0200

    relationships

--- ClassCreator.py
+++ ClassCreator.py
@@ -1,24 +1,42 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from inspect import CO_ASYNC_GENERATOR
+from time import time
+from pymysql import Date
+from sqlalchemy import TIMESTAMP, create_engine
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
-engine = create_engine("mysql://root:****l@localhost/pizzaSQL", echo = True)
+engine = create_engine("mysql://root:123sql@localhost/pizzaSQL", echo = True)
 base = declarative_base()
 connection = engine.connect()
+
+pizza_ingredients = Table(
+    'pizza_ingredients', base.metadata,
+    Column('pizza_id', Integer, ForeignKey('pizzas.id')),
+    Column('ingredient_id', Integer, ForeignKey('ingredients.id'))
+)
+
+order_pizzas = Table(
+    'order_pizzas', base.metadata,
+    Column('order_id', Integer, ForeignKey('orders.id')),
+    Column('pizza_id', Integer, ForeignKey('pizzas.id'))
+)
 
 
 class Pizza(base):
 
-    __tablename__ = 'pizza'
+    __tablename__ = 'pizzas'
 
     id = Column(Integer, primary_key = True)
     pizza_name = Column(String(255))
-    price = Column(Integer)
-  
+    ingredients = relationship('Ingredient', secondary = pizza_ingredients)
 
-"""  
-class Ingredients(base):
+    def __init__(self, pizza_name):
+        self.pizza_name = pizza_name
+  
+ 
+class Ingredient(base):
 
     __tablename__ = 'ingredients'
 
@@ -26,30 +44,27 @@ class Ingredients(base):
     ingredient_name = Column(String(255))
     price = Column(Integer)
 
+    def __init__(self, ingredient_name, price):
+        self.ingredient_name = ingredient_name
+        self.price = price
+
+
 class Order(base):
 
-    __tablename__ = 'order'
+    __tablename__ = 'orders'
 
     order_id = Column(Integer, primary_key = True)
-    pizza_id = Column(Integer, ForeignKey('pizza.id'))
-    pizza = relationship("Pizza")
+    order_time = Column(TIMESTAMP)
+    pizzas = relationship('Pizza', secondary = order_pizzas)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
 
 class Customer(base):
 
-    __tablename__ = 'customer'
+    __tablename__ = 'customers'
 
     name = Column(String(255))
     address_id = Column(Integer, ForeignKey('address.id'))
-    address = relationship("Address")
+    address = relationship('Address')
 
-
-class Address(base):
-    __tablename__ = 'address'
-
-    id = Column(Integer, primary_key = True)
-    city_name = Column(String(255))
-    street_name = Column(String(255))
-    zip_code = Column(String(255))
-"""
     
 base.metadata.create_all(engine)
